505126cf10d8efd093fdfe8ef9f26dccdd29cee4
Agus S. Mubarok <gusrylmubarok@gmail.com>
2022-07-05 16:41:48 +0700

    Make Model and Connection to Database

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"github.com/gusrylmubarok/alpha-ecommerce-goapi/controllers"
+	"github.com/gusrylmubarok/alpha-ecommerce-goapi/database"
+	"github.com/gusrylmubarok/alpha-ecommerce-goapi/middleware"
+	"github.com/gusrylmubarok/alpha-ecommerce-goapi/routes"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
 	
 }
